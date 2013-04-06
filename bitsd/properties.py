b96ed6cf7de5582fbae800eba1456345360e3027
Stefano Sanfilippo <a.little.coder@gmail.com>
2013-04-06 22:27:45 +0200

    Renaming fonera_port option to remote_port

--- bitsd/properties.py
+++ bitsd/properties.py
@@ -2,6 +2,6 @@ from tornado.options import define
 
 define("web_port", default=8008, help="Web server port")
 define("ws_port", default=3389, help="WebSocket server port")
+define("remote_port", default=8888, help="Port for fonera server.")
 
-define("fonera_port", default=8888, help="Port for fonera server.")
 define("fonera_address", default="127.0.0.1", help="Fonera IP address.")
