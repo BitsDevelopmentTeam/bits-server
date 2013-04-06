1300be92a6602ae9439bafe3e8d332661a5496df
Stefano Sanfilippo <a.little.coder@gmail.com>
2013-04-06 22:18:10 +0200

    Moving websockets.MessageNotifier to submodule to avoid circular dependency.

--- bitsd/websockets/__init__.py
+++ bitsd/websockets/__init__.py
@@ -11,25 +11,3 @@ def startserver():
         (r'/', StatusHandler)
     ])
     server.listen(options.ws_port)
-
-
-class MessageNotifier(object):
-    """Keeps a list of WebSocket handlers to notify with a message."""
-    def __init__(self, name):
-        self.clients = []
-        self.name = name
-
-    def register(self, client):
-        """Add a new handler to the clients list."""
-        LOG.debug('Adding client {} to {}'.format(client, self.name))
-        self.clients.append(client)
-
-    def unregister(self, client):
-        """Remove the handler from the clients list."""
-        LOG.debug('Removing client {} from {}'.format(client, self.name))
-        self.clients.remove(client)
-
-    def broadcast(self, message):
-        """Notify all clients."""
-        for client in self.clients:
-            client.write_message(message)
