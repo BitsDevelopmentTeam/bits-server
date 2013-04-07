7ce625cc4b1f5bd99a690d72a63bcfcd644e27d5
Stefano Sanfilippo <a.little.coder@gmail.com>
2013-04-07 20:30:41 +0200

    Added unbase64 utility.

--- bitsd/common.py
+++ bitsd/common.py
@@ -7,6 +7,15 @@
 #
 
 import logging
+import base64
 
 # Get logger
 LOG = logging.getLogger('tornado.general')
+
+def unbase64(message):
+    try:
+        decodedmex = base64.b64decode(message)
+    except TypeError:
+        LOG.error('Received message is not valid base64: {!r}'.format(message))
+    else:
+        return decodedmex.decode('utf8')
