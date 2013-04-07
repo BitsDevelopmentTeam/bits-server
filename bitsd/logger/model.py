d377bd6b0d0e240d0aab7fc23824de6cfc54cb33
Stefano Sanfilippo <a.little.coder@gmail.com>
2013-04-07 17:01:23 +0200

    Implemented logger models.

--- bitsd/logger/model.py
+++ bitsd/logger/model.py
@@ -7,21 +7,33 @@
 #
 
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.types import Column, Integer, Float, DateTime
+from sqlalchemy.types import Column, Integer, Float, DateTime, Enum
+
+import datetime
 
 Base = declarative_base()
 
 class TemperatureSample(Base):
     __tablename__ = 'Temperature'
 
-    timestamp = Column(DateTime, primary_key=True)
+    timestamp = Column(DateTime, primary_key=True, default=datetime.datetime.utcnow)
     value = Column(Float, primary_key=True)
     sensor = Column(Integer)
-    #modified_by = TODO
+    modified_by = Enum('BITS', 'web')
+
+    def __init__(self, value, sensor, modified_by):
+        self.value = value
+        self.sensor = sensor
+        self.modified_by = modified_by
+
 
 class Status(Base):
     __tablename__ = 'Status'
 
-    timestamp = Column(DateTime, primary_key=True)
-    #status = TODO
-    #modified_by = TODO
+    timestamp = Column(DateTime, primary_key=True, default=datetime.datetime.utcnow)
+    status = Enum('open', 'closed')
+    modified_by = Enum('BITS', 'web')
+
+    def __init__(self, status, modified_by):
+        self.status = status
+        self.modified_by = modified_by
