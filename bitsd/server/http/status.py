37d38c38ea531be23ff8cc1f6440151072f1940d
Stefano Sanfilippo <a.little.coder@gmail.com>
2013-04-08 23:41:53 +0200

    Adding status handler.

---
+++ bitsd/server/http/status.py
@@ -0,0 +1,17 @@
+#
+# Copyright (C) 2013 Stefano Sanfilippo
+# Copyright (C) 2013 BITS development team
+#
+# This file is part of bitsd, which is released under the terms of
+# GNU GPLv3. See COPYING at top level for more information.
+#
+
+import tornado.web
+
+from bitsd.logger import get_current_status
+
+class StatusPageHandler(tornado.web.RequestHandler):
+    """Get a single digit, indicating BITS status (open/closed)"""
+    def get(self):
+        status = get_current_status()
+        self.write('1' if status.value == 'open' else '0')
