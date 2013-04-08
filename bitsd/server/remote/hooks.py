0d6bae2fd7f78be02324f37ebf81c6c98806121e
Stefano Sanfilippo <a.little.coder@gmail.com>
2013-04-08 19:38:22 +0200

    Implementing handle_{temperature,status}_command, adding other stubs.

--- bitsd/server/remote/hooks.py
+++ bitsd/server/remote/hooks.py
@@ -9,34 +9,39 @@
 # NOTE: don't forget to register your handler in RemoteHandler.ACTIONS!
 
 from bitsd.common import LOG, unbase64
-
+from bitsd.logger import log_temperature, log_status
 
 def handle_temperature_command(sensorid, value):
     sensorid = int(sensorid)
     value = float(value)
     LOG.info('Received temperature: sensorid={}, value={}'.format(sensorid, value))
+    log_temperature(sensorid, value, 'BITS')
 
 
 def handle_status_command(status):
     status = int(status)
     LOG.info('Received status: {}'.format(status))
+    log_status('open' if status == 1 else 'close', 'BITS')
 
 
 def handle_enter_command(id):
     id = int(id)
     LOG.info('Received enter command: id={}'.format(id))
+    LOG.warning('handle_enter_command not implemented.')
 
 
 def handle_leave_command(id):
     id = int(id)
     LOG.info('Received leave command: id={}'.format(id))
+    LOG.warning('handle_leave_command not implemented.')
 
 
 def handle_message_command(message):
     message = unbase64(message)
     LOG.info('Received message command: message={!r}'.format(message))
-
+    LOG.warning('handle_message_command not implemented.')
 
 def handle_sound_command(id):
     id = int(id)
     LOG.info('Received sound command: id={}'.format(id))
+    LOG.warning('handle_sound_command not implemented.')
