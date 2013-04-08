777f107b5b38e3832cffa5375149f06ab81fcba0
Stefano Sanfilippo <a.little.coder@gmail.com>
2013-04-08 23:15:27 +0200

    Adding signal handling code to main.

--- bitsd/main.py
+++ bitsd/main.py
@@ -14,16 +14,45 @@ import bitsd.server.websockets as websockets
 import bitsd.server.remote as remote
 import bitsd.logger as logger
 
+from bitsd.common import LOG
+
 from tornado.options import parse_command_line
 import tornado.ioloop
 
+import signal
+import time
+
+# Signal code adapted from https://gist.github.com/kachayev/1387470
+
+def sig_handler(sig, frame):
+    """Catch signal and init callback.
+
+    Reference:
+    http://codemehanika.org/blog/2011-10-28-graceful-stop-tornado.html
+    """
+    LOG.warning('Caught signal: %s', sig)
+    tornado.ioloop.IOLoop.instance().add_callback(shutdown)
+
+
+def shutdown():
+    """Stop server and add callback to stop i/o loop."""
+    io_loop = tornado.ioloop.IOLoop.instance()
+    LOG.info('Shutting down in 2 seconds')
+    io_loop.add_timeout(time.time() + 2, io_loop.stop)
+
+
 def main():
+    """Entry point for bitsd."""
     parse_command_line()
 
     logger.startdb()
     http.startserver()
     websockets.startserver()
     remote.startserver()
+
+    # Add signal handlers...
+    signal.signal(signal.SIGTERM, sig_handler)
+    signal.signal(signal.SIGINT, sig_handler)
 
     tornado.ioloop.IOLoop.instance().start()
 
