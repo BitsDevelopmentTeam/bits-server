81b13e4a64ba621db698d621bfdb6025c30ffb3d
Stefano Sanfilippo <a.little.coder@gmail.com>
2013-04-10 21:51:40 +0200

    Adding bitsd.persistence.pages with dummy model.

---
+++ bitsd/persistence/pages/__init__.py
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2013 Stefano Sanfilippo
+# Copyright (C) 2013 BITS development team
+#
+# This file is part of bitsd, which is released under the terms of
+# GNU GPLv3. See COPYING at top level for more information.
+#
+
+from .model import Page
+
+
+def get_page(slug):
+    return Page()
