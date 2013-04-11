6a3784acf8f8cccba463ba08fc65962acdd4d79f
Stefano Sanfilippo <a.little.coder@gmail.com>
2013-04-11 23:21:33 +0200

    Disallow opening/closing BITS two times in a row.

--- bitsd/listener/remote/hooks.py
+++ bitsd/listener/remote/hooks.py
@@ -13,7 +13,8 @@ Hooks called by bitsd.listener.remote.RemoteListener to handle commands.
 # NOTE: don't forget to register your handler in RemoteListener.ACTIONS!
 
 from bitsd.common import LOG
-from bitsd.persistence.logger import log_temperature, log_status, get_latest_data
+from bitsd.persistence.logger import log_temperature, log_status
+from bitsd.persistence.logger import get_latest_data, get_current_status
 from bitsd.server.websockets.status import broadcast_status
 from bitsd.client.fonera import Fonera
 # Fixing Sphinx complaints
@@ -35,8 +36,7 @@ def handle_temperature_command(sensorid, value):
         return
 
     temp = log_temperature(value, sensorid, 'BITS')
-    # Return wrapped in dict
-    broadcast_status(temp.jsondict(wrap=True))
+    broadcast_status(temp.jsondict(wrap=True)) # wrapped in a dict
 
 
 
@@ -52,9 +52,12 @@ def handle_status_command(status):
         return
 
     textstatus = 'open' if status == 1 else 'closed'
-    status = log_status(textstatus, 'BITS')
-    # Return wrapped in dict
-    broadcast_status(status.jsondict(wrap=True))
+    curstatus = get_current_status()
+    if curstatus.value != textstatus:
+        status = log_status(textstatus, 'BITS')
+        broadcast_status(status.jsondict(wrap=True)) # wrapped in a dict
+    else:
+        LOG.error('Cannot open/close BITS two times in a row! Ignoring.')
 
 
 def handle_enter_command(id):
