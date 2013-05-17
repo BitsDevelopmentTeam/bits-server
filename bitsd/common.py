27a7723f048a585eca617477e16b69dbc3dbc721
Otacon22 <danieleiamartino@gmail.com>
2013-05-17 15:34:08 +0200

    Added mode option for chmod on te unix socket file

--- bitsd/common.py
+++ bitsd/common.py
@@ -19,7 +19,7 @@ import os
 LOG = logging.getLogger('tornado.general')
 
 
-def bind(server, port, usocket, address=None):
+def bind(server, port, usocket, address=None, mode=None):
     """Make server listen on port (inet socket).
     If given, prefer `usocket`, path to a unix socket.
     The latter is useful for reverse proxying.
@@ -34,7 +34,7 @@ def bind(server, port, usocket, address=None):
     if usocket:
         LOG.info('Starting on unix socket `{}`'.format(usocket))
         try:
-            socket = bind_unix_socket(usocket)
+            socket = bind_unix_socket(usocket, mode)
             os.chown(usocket, options.usocket_uid, options.usocket_gid)
         except OSError as error:
             LOG.error('Cannot create unix socket: {}'.format(error))
