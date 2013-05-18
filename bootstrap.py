5dbd3ca30ac87a3de2b6d8a44dda1530de1193b6
Stefano Sanfilippo <a.little.coder@gmail.com>
2013-05-18 20:34:36 +0200

    Added bootstrap script to load .md pages in DB

---
+++ bootstrap.py
@@ -0,0 +1,25 @@
+#! /usr/bin/env python
+#
+# Copyright (C) 2013 Stefano Sanfilippo
+# Copyright (C) 2013 BITS development team
+#
+# This file is part of bitsd, which is released under the terms of
+# GNU GPLv3. See COPYING at top level for more information.
+#
+
+"""
+Fill Database with Markdown pages.
+"""
+
+import bitsd.properties
+
+from bitsd.persistence import start, persist
+from bitsd.persistence.pages.model import Page
+
+
+if __name__ == '__main__':
+    start()
+
+    with open('INFO.md', 'r') as info:
+        infopage = Page('Info', info.read().decode('utf-8'))
+        persist(infopage)
