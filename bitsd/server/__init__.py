ce2005be6e33c71da0f14240d339180d791afff7
Stefano Sanfilippo <a.little.coder@gmail.com>
2013-09-29 16:07:56 +0200

    Adding missing trailing comma.

--- bitsd/server/__init__.py
+++ bitsd/server/__init__.py
@@ -30,11 +30,10 @@ def start():
             (r'/status', handlers.StatusPageHandler),
             (r'/data', handlers.DataPageHandler),
             (r'/(info)', handlers.MarkdownPageHandler),
-            (r'/ws', handlers.StatusHandler)
+            (r'/ws', handlers.StatusHandler),
             (r'/login', handlers.LoginPageHandler),
             (r'/logout', handlers.LogoutPageHandler),
             (r'/admin', handlers.AdminPageHandler),
-
         ],
         ui_modules=uimodules,
         gzip=True,
