bc5183770e621e220ceb0d600c12f5690ef02611
thypon <miwaxe+git@gmail.com>
2013-11-08 18:00:37 +0100

    Added TitleEventListener to all the pages

---
+++ bitsd/server/script/other.ts
@@ -0,0 +1,21 @@
+/// <reference path="helpers/zepto.d.ts" />
+
+import ws = require("websocket");
+import v = require("view");
+import c = require("controller");
+import m = require("model");
+import debug = require("debug");
+
+$(function() {
+    var controller = new c.Controller(),
+        titleListener: m.IEventListener = v.TitleEventListener.create(),
+        socket = new ws.Socket(/https/.test(location.protocol)? "wss": "ws" + "://" + location.hostname + ":" + location.port +"/ws");
+
+    debug.logger.level = $("meta[name='mode']").attr("content") || "production";
+
+    controller.register(titleListener);
+
+    socket.onmessage = (event) => controller.handleUpdate($.parseJSON(event.data));
+
+    socket.onerror = (event) => debug.logger.error("WS Error", event);
+});
